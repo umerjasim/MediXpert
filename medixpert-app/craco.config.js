5587bf8a614bd269886e652d7c49fac33f78ed3b
umer.j <umer.j@travancoreanalytics.com>
2025-03-03 14:43:50 +0530

    render

--- medixpert-app/craco.config.js
+++ medixpert-app/craco.config.js
@@ -1,30 +1,51 @@
-// craco.config.js
+// // craco.config.js
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+
+// module.exports = {
+//   webpack: {
+//     plugins: [
+//       new WorkboxPlugin.GenerateSW({
+//         clientsClaim: true,
+//         skipWaiting: true,
+//         exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+//         runtimeCaching: [
+//           {
+//             urlPattern: /\.(?:png|jpg|jpeg|gif|webp|svg|js|css|html)$/,
+//             handler: 'CacheFirst',
+//             options: {
+//               cacheName: 'assets',
+//               expiration: {
+//                 maxEntries: 50,
+//                 maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+//               },
+//             },
+//           },
+//         ],
+//       }),
+//     ],
+//     configure: (webpackConfig) => {
+//       // Remove CRA's default GenerateSW plugin
+//       webpackConfig.plugins = webpackConfig.plugins.filter(
+//         (plugin) => plugin.constructor.name !== 'GenerateSW'
+//       );
+//       return webpackConfig;
+//     },
+//   },
+// };
+
+
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   webpack: {
     plugins: [
-      new WorkboxPlugin.GenerateSW({
-        clientsClaim: true,
-        skipWaiting: true,
-        exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-        runtimeCaching: [
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|gif|webp|svg|js|css|html)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'assets',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-              },
-            },
-          },
-        ],
+      new WorkboxPlugin.InjectManifest({
+        swSrc: './src/service-worker.ts', // Your custom service worker
+        swDest: 'service-worker.js',
       }),
     ],
     configure: (webpackConfig) => {
-      // Remove CRA's default GenerateSW plugin
+      // Remove CRA's default GenerateSW plugin if it exists
       webpackConfig.plugins = webpackConfig.plugins.filter(
         (plugin) => plugin.constructor.name !== 'GenerateSW'
       );
